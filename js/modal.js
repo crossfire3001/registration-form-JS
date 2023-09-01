bbfe6681192a40fa91769b932e19ab1c75af7b78
crossfire3001 <obiwan2035@mail.ru>
2023-09-01 17:06:01 +0500

    modal and registration form updates

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+
+
+    /* if (!orderFilled) {
+    [productInput, addressInput, phoneInput].forEach((item) => {
+        item.value = "";
+    });
+    alert("Спасибо за заказ!");
+} */
+
+    /* baseInputs.forEach((input) => {
+         if(!inputFilled.value) {
+             console.log('Заполните все поля');
+         }
+    });
+
+     if (!inputFilled) {
+         baseInputs.forEach((item) => {
+             item.value = "";
+         });
+         alert("Спасибо за заказ!");
+     } */
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 window.onload = function () {
-    const fullName = document.getElementById('fullname-input');
-    const userName = document.getElementById('username-input');
+    const fullNameInput = document.getElementById('fullname-input');
+    const userNameInput = document.getElementById('username-input');
     const checkBox = document.getElementById('checkbox');
+    const registrationBtn = document.querySelector('.registration__btn');
 
 
     //2. В поле "Full Name" запретите вводить цифры.
-    fullName.onkeydown = (e) => {
+    fullNameInput.onkeydown = (e) => {
         const letters = e.key;
         if (!isNaN(letters)) {
             e.preventDefault();
@@ -13,7 +14,7 @@ window.onload = function () {
     }
 
     // 3. В поле "Your username" запретите вводить точки и запятые.
-    userName.onkeydown = (e) => {
+    userNameInput.onkeydown = (e) => {
         const dots = e.key;
         if (dots === ',' || dots === '.') {
             e.preventDefault();
@@ -27,6 +28,42 @@ window.onload = function () {
             console.log('Согласен');
         } else {
             console.log('Не согласен')
+        }
+    }
+
+    registrationBtn.onclick = (e) => {
+        const emailInput = document.getElementById('email-input');
+        const passwordInput = document.getElementById('password-input');
+        const repeatPasswordInput = document.getElementById('repeat-password-input');
+        const inputFilled = false;
+
+        if (!fullNameInput.value) {
+            alert('Заполните имя');
+            return;
+        }
+        if (!userNameInput.value) {
+            alert('Заполните заполните ник пользователя');
+            return;
+        }
+        if (!emailInput.value) {
+            alert('Заполните почту');
+            return;
+        }
+        if (!passwordInput.value || passwordInput.value.length <= 8) {
+            alert('Заполните пароль');
+            return;
+        }
+        if (!repeatPasswordInput.value || repeatPasswordInput.value.length <= 8) {
+            alert('Заполните пароль повторно');
+            return;
+        }
+        if (passwordInput.value !== repeatPasswordInput.value) {
+            alert('Пароли не совпадают');
+            return;
+        }
+        if (!checkBox.checked) {
+            alert('Подтвердите условия соглашения');
+            return;
         }
     }
 
